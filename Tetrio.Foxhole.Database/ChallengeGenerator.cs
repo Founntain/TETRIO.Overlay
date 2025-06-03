052896b4e528b71f3965e2bf5f318716dd73db5a
founntain <7@founntain.dev>
2025-06-03 19:36:57 +0200

    small changes and forgor one part for finesse removal 💀

--- Tetrio.Foxhole.Database/ChallengeGenerator.cs
+++ Tetrio.Foxhole.Database/ChallengeGenerator.cs
@@ -18,6 +18,13 @@ public class ChallengeGenerator
         _random = new(seed);
     }
 
+    public ChallengeGenerator(int seed)
+    {
+        _day = DateTime.UtcNow;
+
+        _random = new(seed);
+    }
+
     public async Task<List<Challenge>> GenerateChallengesForDay(TetrioContext context)
     {
         List<Challenge> challenges =
@@ -80,76 +87,7 @@ public class ChallengeGenerator
         };
     }
 
-
-    private async Task<Challenge> GenerateReverseChallenge(TetrioContext context)
-    {
-        var challengeConditions = new List<ChallengeCondition>();
-        var height = 150;
-        var yesterdayChallenge = await context.Challenges.Where(x => x.Points == (byte)Difficulty.Reverse).OrderByDescending(x => x.Date).FirstOrDefaultAsync();
-        var randomMod = GetRandomReverseMod(yesterdayChallenge?.Mods);;
-
-        height += randomMod.HeightModifier;
-
-        challengeConditions.Add(new () { Type = ConditionType.Height, Value = height});
-
-        return new Challenge
-        {
-            Date = DateOnly.FromDateTime(_day.Date),
-            Points = (byte) Difficulty.Reverse,
-            Mods = randomMod.Mod,
-            Conditions = challengeConditions.ToHashSet()
-        };
-    }
-
-    private (string Mod, int HeightModifier) GetRandomReverseMod(string? challenge)
-    {
-        var yesterdaysReverseMod = challenge;
-
-        (string, int)? selectedMod = null;
-
-        var tries = 0;
-
-        // If after 300 tries we still don't find a mod that is different from yesterday's one, we just use the one rolled last
-        while (tries <= 300)
-        {
-            tries++;
-
-            var mod = _random.Next(0, 8);
-
-            switch (mod)
-            {
-                case 0:
-                    selectedMod = ("expert_reversed", _random.Next(0, 50)); break;
-                case 1:
-                    selectedMod = ("nohold_reversed", _random.Next(0, 150)); break;
-                case 2:
-                    selectedMod = ("messy_reversed", _random.Next(0, 200)); break;
-                case 3:
-                    selectedMod = ("gravity_reversed", _random.Next(0, 200)); break;
-                case 4:
-                    selectedMod = ("volatile_reversed", _random.Next(0, 400)); break;
-                case 5:
-                    selectedMod = ("doublehole_reversed", _random.Next(0, 100)); break;
-                case 6:
-                    selectedMod = ("invisible_reversed", _random.Next(0, 50)); break;
-                case 7:
-                    selectedMod = ("allspin_reversed", _random.Next(0, 200)); break;
-                // We default to reverse volatile, as it is the easiest for most.
-                // However, the default case should never trigger.
-                default: selectedMod = ("volatile_reversed", _random.Next(0, 400)); break;
-            }
-
-            // We generate mods until we find a reverse mod that is different from yesterdays one
-            if (selectedMod.Value.Item1 != yesterdaysReverseMod) break;
-        }
-
-        // Just as a fallback, if selectedMod is still null, we just use the default case.
-        selectedMod ??= ("volatile_reversed", _random.Next(0, 400));
-
-        return selectedMod.Value;
-    }
-
-    private async Task<Challenge> GenerateChallenge(Difficulty difficulty, TetrioContext context)
+    public async Task<Challenge> GenerateChallenge(Difficulty difficulty, TetrioContext context)
     {
         var challengeConditions = new List<ChallengeCondition>();
 
@@ -224,6 +162,74 @@ public class ChallengeGenerator
             Mods = mods,
             Conditions = challengeConditions.ToHashSet()
         };
+    }
+
+    private async Task<Challenge> GenerateReverseChallenge(TetrioContext context)
+    {
+        var challengeConditions = new List<ChallengeCondition>();
+        var height = 150;
+        var yesterdayChallenge = await context.Challenges.Where(x => x.Points == (byte)Difficulty.Reverse).OrderByDescending(x => x.Date).FirstOrDefaultAsync();
+        var randomMod = GetRandomReverseMod(yesterdayChallenge?.Mods);;
+
+        height += randomMod.HeightModifier;
+
+        challengeConditions.Add(new () { Type = ConditionType.Height, Value = height});
+
+        return new Challenge
+        {
+            Date = DateOnly.FromDateTime(_day.Date),
+            Points = (byte) Difficulty.Reverse,
+            Mods = randomMod.Mod,
+            Conditions = challengeConditions.ToHashSet()
+        };
+    }
+
+    private (string Mod, int HeightModifier) GetRandomReverseMod(string? challenge)
+    {
+        var yesterdaysReverseMod = challenge;
+
+        (string, int)? selectedMod = null;
+
+        var tries = 0;
+
+        // If after 300 tries we still don't find a mod that is different from yesterday's one, we just use the one rolled last
+        while (tries <= 300)
+        {
+            tries++;
+
+            var mod = _random.Next(0, 8);
+
+            switch (mod)
+            {
+                case 0:
+                    selectedMod = ("expert_reversed", _random.Next(0, 50)); break;
+                case 1:
+                    selectedMod = ("nohold_reversed", _random.Next(0, 150)); break;
+                case 2:
+                    selectedMod = ("messy_reversed", _random.Next(0, 200)); break;
+                case 3:
+                    selectedMod = ("gravity_reversed", _random.Next(0, 200)); break;
+                case 4:
+                    selectedMod = ("volatile_reversed", _random.Next(0, 400)); break;
+                case 5:
+                    selectedMod = ("doublehole_reversed", _random.Next(0, 100)); break;
+                case 6:
+                    selectedMod = ("invisible_reversed", _random.Next(0, 50)); break;
+                case 7:
+                    selectedMod = ("allspin_reversed", _random.Next(0, 200)); break;
+                // We default to reverse volatile, as it is the easiest for most.
+                // However, the default case should never trigger.
+                default: selectedMod = ("volatile_reversed", _random.Next(0, 400)); break;
+            }
+
+            // We generate mods until we find a reverse mod that is different from yesterdays one
+            if (selectedMod.Value.Item1 != yesterdaysReverseMod) break;
+        }
+
+        // Just as a fallback, if selectedMod is still null, we just use the default case.
+        selectedMod ??= ("volatile_reversed", _random.Next(0, 400));
+
+        return selectedMod.Value;
     }
 
     private async Task<string> GenerateModsForChallenge(TetrioContext context, Difficulty difficulty)
@@ -327,6 +333,7 @@ public class ChallengeGenerator
         var allConditions = Enum.GetValues<ConditionType>().ToList();
 
         allConditions.RemoveAt(0);
+        allConditions.Remove(ConditionType.Finesse);
 
         allConditions.Remove(ConditionType.AllClears);
 
